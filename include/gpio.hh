8aef9f8426e097576aaa03a22b3f61cd3839e018
brothaman <rbrothers12@gmail.com>
2022-04-17 11:52:03 -0500

    Added a GPIO library with containers for pins
    * Added support for control general purpose input output pins
      on various AVR MCUs.
    * Added support for ATMega32u4 (untested)
    * Added support for simple gpio actions
    * Added implement method to store PORTn and DDRx in data member of
      GPIO class
    * added function definitions for `GPIO` class
    * Tested and verified on Arduino Mega 2560 R3

---
+++ include/gpio.hh
@@ -0,0 +1,54 @@
+#ifndef GPIO_HH
+#define GPIO_HH
+
+
+#include "avr/io.h"
+
+#define INPUT   0
+#define OUTPUT  1
+#define ON 		1
+#define OFF 	0
+
+/**
+ * GPIO Pin
+ * A class to generate pin objects.
+ * Each pin Consist of
+ * @data pin - the pin ex. PB7
+ * @data ddr - the data direction register (ex. DDRB)
+ * @data mode - the mode of the pin (ex. OUTPUT or INPUT
+ */
+class GPIOPin
+{
+    public:
+        /* CONSTRUCTOR */
+        GPIOPin(volatile uint8_t * _ddr, volatile uint8_t * _port, uint8_t _pin);
+
+        /* GPIO BEHAVIOR */
+        void toggle(void);
+
+        /* PIN CONFIG */
+        void pinIOWrite(uint8_t _state);
+
+        /* SETTERS */
+        void setMode(uint8_t _mode);
+
+        /* GETTERS */
+        uint8_t getPin(void);
+        uint8_t * getDDR(void);
+        uint8_t getMode(void);
+
+    private:
+        uint8_t pin;
+        volatile uint8_t * ddr;
+        volatile uint8_t * port;
+        uint8_t mode;
+        uint8_t state;
+
+        /* SETTERS */
+        void setPin(uint8_t _pin);
+        void setPort(volatile uint8_t * _ddr);
+        void setRegister(volatile uint8_t * _ddr);
+
+};
+
+#endif // GPIO_HH
--- src/gpio.cc
+++ src/gpio.cc
@@ -1,1 +1,106 @@
 #include <gpio.hh>
+#include <assert.h>
+
+/* CONSTRUCTOR */
+GPIOPin::GPIOPin(volatile uint8_t * _ddr, volatile uint8_t * _port, uint8_t _pin)
+{
+    setRegister(_ddr);
+    setPort(_port);
+    setPin(_pin);
+}
+
+/* METHOD DEFINITIONS */
+
+/**
+ * setPin
+ *
+ * Sets the pin of the object. This should only be called once at instantiation
+ *
+ * @param _pin - GPIO pin represented by the object (ex. PB7)
+ */
+void GPIOPin::setPin(uint8_t _pin)
+{
+    this->pin = _pin;
+}
+
+/**
+ * setPort
+ *
+ * Sets the port. This should only be called once at instantiation
+ *
+ * @param _port - pointer to the port
+ */
+void GPIOPin::setPort(volatile uint8_t * _port)
+{
+    this->port = _port;
+}
+
+/**
+ * setRegister
+ *
+ * Sets the data direction register. THIS REALLY SHOULD ONLY BE CALLED AT
+ * INSTATIATION
+ *
+ * @param _ddr -  pointer to the Data Direction Register. This should only be
+ * called once at instantiation
+ */
+void GPIOPin::setRegister(volatile uint8_t * _ddr)
+{
+    this->ddr = _ddr;
+}
+
+/**
+ * setMode
+ *
+ * Sets the Mode or direction of the data direction register
+ *
+ * @param _mode - Direction of the register (INPUT or OUTPUT)
+ */
+void GPIOPin::setMode(volatile uint8_t _mode)
+{
+    this->mode = _mode;
+
+    // set the direction of the data direction register
+    assert(_mode == INPUT || _mode == OUTPUT);
+    if (this->mode == INPUT)
+    {
+        *this->ddr &= ~(1 << this->pin);
+        
+    }
+    else
+    {
+        *this->ddr |= (1 << this->pin);
+    }
+}
+
+/**
+ * toggle
+ *
+ * Toggles the pin using XOR
+ */
+void GPIOPin::toggle(void)
+{
+		*this->port ^= (1 << this->pin);
+}
+
+/**
+ * pinIOWrite
+ *
+ * Writes a value of ON or OFF to the port
+ *
+ * @param _state - ON or OFF
+ */
+void GPIOPin::pinIOWrite(uint8_t _state)
+{
+		assert(_state == ON || _state == OFF);
+		this->state = _state;
+
+		if (this->state)
+		{
+				*this->port &= ~(1 << this->pin);
+		}
+		else
+		{
+				*this->port |= (1 << this->pin);
+		}
+}
