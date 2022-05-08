ca02ab38261854f65aaeb923fb5e5cca8618e92a
brothaman <rbrothers12@gmail.com>
2022-05-08 12:18:01 -0500

    Adding GPLv2 Licence and WIP changes
    * Added a GPLv2 LICENCE to the project. This is subject to change in the
      future if the project gains traction and needs to be used elsewhere or
      in a different capacity.
    * I dont like mixing changes but its too late now

--- include/comms.hh
+++ include/comms.hh
@@ -1,3 +1,23 @@
+/**
+ * GPIO - Tools for AVR MCU UART, I2C, and SPI communications protocols
+ * Copyright (C) 2022  Robert Brothers
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
 #ifndef COMMS_HH
 #define COMMS_HH
 
@@ -10,6 +30,11 @@
 class BaseSerial
 {
     public:
+        /**
+         * init
+         *
+         * Initialize the comm port
+         */
         void init(uint16_t _baud);
         void setBaud(uint16_t _baud);
         short int getBaud(void);
@@ -22,6 +47,23 @@ class BaseSerial
         int loadBuffer(char * buffer, int len);
 
     private:
+        /**
+         * setUBRRH
+         *
+         * set the high byte UART BAUD Rate Register
+         *
+         * @param
+         */
+        void setUBRRH(volatile uint8_t * _ubrrh);
+
+        /**
+         * setUBRRL
+         *
+         * set the low byte of UART BAUD Rate Register
+         *
+         * @param _ubrrl - pointer to UBRRL
+         */
+        void setUBRRL(volatile uint8_t * _ubrrl);
         /* UART baud rate register high */
         volatile uint8_t * ubrrh;
 
